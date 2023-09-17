a0bac660faaebc01e99825279bc2138899861398
Sanush Shakya <shakyasanush7@gmail.com>
2023-09-17 22:13:18 +0545

    sanush- reservation model changed and got it working with frontend request

--- backend/reservation_app/models.py
+++ backend/reservation_app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Reservation(models.Model):
     reservation_id = models.AutoField(primary_key=True)
-    customer_name = models.CharField(max_length=100)
-    customer_email = models.EmailField()
-    customer_phn =  models.CharField(max_length=20)
-    customer_requirement = models.TextField()
+    check_in = models.DateField(default=timezone.now)  # Set default to the current date
+    check_out = models.DateField(default=timezone.now)
+    room_type =  models.CharField(max_length=20)
+    email = models.EmailField()
--- backend/reservation_app/serializers.py
+++ backend/reservation_app/serializers.py
@@ -5,4 +5,3 @@ class ReservationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Reservation
         fields = '__all__'
-        #('reservation_id', 'customer_name','customer_email','customer_phn','customer_requirement')
